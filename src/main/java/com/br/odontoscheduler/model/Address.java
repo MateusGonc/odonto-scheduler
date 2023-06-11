a2f8d4f1c0ae872373e16746215a53d9db65cabe
Mateus Goncalves <mateus.goncalves@projecti.com.br>
2023-06-11 11:12:57 -0300

    ESPM-33 add neighborhood

--- src/main/java/com/br/odontoscheduler/model/Address.java
+++ src/main/java/com/br/odontoscheduler/model/Address.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class Address {
     private String street;
+    private String neighborhood;
     private String number;
     private String zipcode;
     private String city;
